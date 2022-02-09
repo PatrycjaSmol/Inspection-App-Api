7eb16342df66a6f445c266e0be7a4bf09612e1b4
Patrycja Smolińska <54121426+PatrycjaSmol@users.noreply.github.com>
2022-02-09 08:11:11 +0100

    first commit
    first commit

---
+++ InspectionAPI/Data/Data.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+        
+
+        public DbSet<Inspection> Inspections { get; set; }  
+        public DbSet<InspectionType> Types { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
